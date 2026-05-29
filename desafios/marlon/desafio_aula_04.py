2e09770abc286886ea76ba383bfc90fe56dacb8a
Marlon <marlonlucio.academic@gmail.com>
2026-05-29 09:58:18 -0300

    Add files via upload

---
+++ desafios/marlon/desafio_aula_04.py
@@ -0,0 +1,53 @@
+print('{:=^40}'.format('Aluno'))
+print('Aluno : Marlon Lúcio Rosas da Silva')
+print('Prof : Cloves Rocha')
+print('Turma : ADS')
+print('{:=^40}'.format('Expressão 1'))
+print('')
+print('(A > B) and (C >= 10)')
+print('A = 5')
+print('B = 3')
+print('C = 8')
+print('')
+print('(5>3) = True')
+print('8>=10 = False')
+print('(5>3) and (8>=10)')
+print('')
+A = 5
+B = 3
+C = 8
+res = (5>3) and (8>=10)
+print('{:=^40}'.format('Resultado'))
+print(f'O resultado foi {res}')
+print('{:=^40}'.format('Expressão 2'))
+print('')
+print('(A != B) or (B < C)')
+print('A = 5')
+print('B = 3')
+print('C = 8')
+print('')
+print('(5 != 3) or (3 < 8) ')
+A = 5
+B = 3
+C = 8
+res = (5 != 3) or (3 < 8)
+print('{:=^40}'.format('Resultado'))
+print(f'O resultado foi ({res})')
+print('')
+print('{:=^40}'.format('Expressão 3'))
+print('')
+print('A = 5')
+print('B = 3')
+print('C = 8')
+print('')
+print('not (A == 5)')
+A = 5
+B = 3
+C = 8
+res = not (A == 5)
+print('{:=^40}'.format('Resultado'))
+print(f'O resultado foi ({res})')
+
+
+
+
